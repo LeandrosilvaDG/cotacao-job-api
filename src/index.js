605cad5f0bf9831b9d438c6bc6658a426274122c
leandro silva <leandrosilvadg@gmail.com>
2019-08-20 15:00:57 -0300

    Fix: include cors application

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 

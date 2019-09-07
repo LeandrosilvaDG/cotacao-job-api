03afebc1b7c91de2160df40b11b4bb0a47aeec9a
Leandro Silva <leandrosilvadg@gmail.com>
2019-09-07 09:49:15 -0300

    Fix: auth adjust routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -18,19 +18,19 @@ routes.put('/user/:id', controllers.UserController.update)
 routes.delete('/user/:id', controllers.UserController.destroy)
 
 // routes.get('/test', authMiddleware, (req, res) => res.json({ ok: true }))
+routes.get('/questions', controllers.QuestionsController.index)
+routes.get('/questions/:questionId', controllers.QuestionsController.show)
+routes.get('/opportunity', controllers.OpportunityController.index)
+routes.get('/opportunity/:opportunityId', controllers.OpportunityController.show)
 
 routes.use(authMiddleware)
 
 // Questions routers
-routes.get('/questions', controllers.QuestionsController.index)
-routes.get('/questions/:questionId', controllers.QuestionsController.show)
 routes.post('/questions', controllers.QuestionsController.store)
 routes.put('/questions/:questionId', controllers.QuestionsController.update)
 routes.delete('/questions/:questionId', controllers.QuestionsController.delete)
 
 // Opportunity routes
-routes.get('/opportunity', controllers.OpportunityController.index)
-routes.get('/opportunity/:opportunityId', controllers.OpportunityController.show)
 routes.post('/opportunity', controllers.OpportunityController.store)
 routes.put('/opportunity/:opportunityId', controllers.OpportunityController.update)
 routes.delete('/opportunity/:opportunityId', controllers.OpportunityController.delete)
